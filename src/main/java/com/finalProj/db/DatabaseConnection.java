c12dcf7038ad8aaf92abae425d7eb152ba4f09d1
aniyahs <aas278@case.edu>
2024-04-20 17:17:24 -0400

    server connection outline

--- src/main/java/com/finalProj/db/DatabaseConnection.java
+++ src/main/java/com/finalProj/db/DatabaseConnection.java
@@ -1,1 +1,31 @@
- 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String JDBC_URL = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new SQLException("SQL Server JDBC Driver not found.");
+        }
+
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
