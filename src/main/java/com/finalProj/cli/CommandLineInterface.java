6b2519b631e47487d836b8f8390f7d16bc5024da
Mariana Davis <lmd117@case.edu>
2024-04-22 02:36:03 -0400

    Should be working UI, but need to test with working DB

--- src/main/java/com/finalProj/cli/CommandLineInterface.java
+++ src/main/java/com/finalProj/cli/CommandLineInterface.java
@@ -1,15 +1,6 @@
- package main.java.com.finalProj.cli;
+package main.java.com.finalProj.cli;
 
-// This also shows "connect.setAutoCommit(false);" in a line of code. Comment the "connect.commit():" and show
-// that the database is not updated without it. The reason to have auto committing set to false if several statements
-// should succeed before committing all changes to the database. That is, for transaction control. However, often
-// this type of transaction control is handled in stored procedures within the database itself. But, we are not
-// always given options of where to handle transaction control so knowing both ways (within the dbms and within
-// code) is helpful.
-//
-// Edit the connectionUrl in the line below that currently reads "jdbc:sqlserver://cxp-sql-02\\abc123;" and replace
-// abc123 with your network id and the server "cxp-sql-02" with the server you were given in an email from the TA.
-// The server for Fall 2023 is either cxp-sql-02 or cxp-sql-03
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -25,14 +16,6 @@ public class CommandLineInterface {
 // Connect to your database.
 // Replace server name, username, and password with your credentials
     public static void main() throws SQLException {
-        String connectionUrl =
-            "jdbc:sqlserver://cxp-sql-02\\abc123;"  // TODO: Change this line to mathc our server
-            + "database=OnlineOrders;"
-            + "user=dbuser;"
-            + "password=csds341143sdsc;"
-            + "encrypt=true;"
-            + "trustServerCertificate=true;"
-            + "loginTimeout=20;";   // dunno if this is right
 
         //scanner to read input
         Scanner myObj = new Scanner(System.in);
@@ -48,7 +31,6 @@ public class CommandLineInterface {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("");
 
-        //myObj.close();
         // check that name and email are in database
         checkUserInDB(custName, custEmail, myObj);
 
